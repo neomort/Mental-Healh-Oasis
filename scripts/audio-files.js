1de67c1a44d07c1ad8786522df12a04f026827ab
Thais Alvarenga <ata381@nyu.edu>
2021-11-10 23:42:17 +0400

    Organized Audio files

--- scripts/audio-files.js
+++ scripts/audio-files.js
@@ -1,3 +1,83 @@
+
+//---------   NARRATION ------------
+
+// narration for the INTRODUCTION in main page
+const sayIntroduction = new Howl({
+  src: ['./sounds/Narration/Narration_Introduction.mp3'],
+  volume: 0.5
+});
+
+// narration for FOREST scene
+const sayForest = new Howl({
+  src: ['./sounds/Narration/Narration_Forest.mp3'],
+  volume: 0.5
+});
+
+// narration for BEACH scene
+const sayBeach = new Howl({
+  src: ['./sounds/Narration/Narration_Beach.mp3'],
+  volume: 0.5
+});
+
+// narration for MOUNTAIN scene
+const sayMountain = new Howl({
+  src: ['./sounds/Narration/Narration_Mountain.mp3'],
+  volume: 0.5
+});
+
+// narration for PARK scene
+const sayPark = new Howl({
+  src: ['./sounds/Narration/Narration_Park.mp3'],
+  volume: 0.5
+});
+
+// narration for the CONCLUSION 
+const sayConclusion = new Howl({
+  src: ['./sounds/Narration/Narration_Conclusion.mp3'],
+  volume: 0.5
+});
+
+
+//---------   SOUNDSCAPE  ------------
+
+// Forest soundscape
+
+const forestSound = new Howl({
+  src: ['./sounds/Forest/Forest.mp3'],
+  volume: 0.5
+});
+
+// Beach soundscape
+
+
+// Mountain soundscape
+
+
+// Park soundscape
+
+const parkSound = new Howl({
+  src: ['./sounds/Park/Park.mp3'],
+  volume: 0.5
+});
+
+//---------   FOREST audio  -----------
+
+//---------   BEACH audio  ------------
+
+//---------   MOUNTAIN audio  ---------
+
+
+//---------   PARK audio  -------------
+
+// audio file for DOG
+const dog = new Howl({
+  src: ['./sounds/Park/dog.mp3'],
+  volume: 0.5
+});
+
+
+//---------   EXTRA audio  -------------
+
 const ow = new Howl({
   src: ['./sounds/Extra/ow.mp3'],
   volume: 0.5
